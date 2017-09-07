16d4993278ae58d16bbeffa7ac4b42a3b18aca67
Shashank Anantharam <shashank95.a@gmail.com>
2017-09-07 08:17:40 +0530

    Added Registration form

--- app/src/main/java/com/example/shashank_pc/trial/LandingPageActivity.java
+++ app/src/main/java/com/example/shashank_pc/trial/LandingPageActivity.java
@@ -3,6 +3,7 @@ package com.example.shashank_pc.trial;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -14,6 +15,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LandingPageActivity extends AppCompatActivity {
 
@@ -32,10 +35,50 @@ public class LandingPageActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    public void getDialogue(){
+
+        //Pop dialogue
+
+        AlertDialog.Builder mBuilder= new AlertDialog.Builder(this);
+        View mView= getLayoutInflater().inflate(R.layout.dialog_register, null);
+        final EditText mPhone= (EditText) mView.findViewById(R.id.etPhone);
+        final EditText mPass= (EditText) mView.findViewById(R.id.etPass);
+        final EditText mRtPass= (EditText) mView.findViewById(R.id.etRtPass);
+        Button mRegisterButton = (Button) mView.findViewById(R.id.btnRegister);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            /*    if(!GenericFunctions.validatePhone(mPhone.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(),"Enter a valid Phone number", Toast.LENGTH_SHORT).show();
+                }
+                else if(mPass.getText().toString()==""){
+                    Toast.makeText(getApplicationContext(), "Enter a password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    if(mPass.getText().toString()!=mRtPass.getText().toString())
+                        Toast.makeText(getApplicationContext(), "Retype password", Toast.LENGTH_SHORT).show();
+                }
+*/
+            }
+        });
+
+
+        mBuilder.setView(mView);
+        AlertDialog dialog= mBuilder.create();
+        dialog.show();
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        getDialogue();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
