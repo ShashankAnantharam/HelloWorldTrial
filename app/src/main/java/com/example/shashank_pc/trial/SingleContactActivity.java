1b4602998913e954a52a1827b41cf2866d0be4eb
Shashank Anantharam <shashank95.a@gmail.com>
2017-09-18 20:02:09 +0530

    Added Single Contact Activity

---
+++ app/src/main/java/com/example/shashank_pc/trial/SingleContactActivity.java
@@ -0,0 +1,190 @@
+package com.example.shashank_pc.trial;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class SingleContactActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    private String mEntityName;
+    private String mEntityID;
+
+    private char mType;
+
+    private String mUserName;
+    private String mUserID;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_contacts);
+
+        Intent caller = getIntent();
+        mEntityName= caller.getStringExtra("Name");
+        mEntityID=caller.getStringExtra("ID");
+        mType=caller.getCharExtra("Type", ' ');
+
+        mUserName=caller.getStringExtra("Username");
+        mUserID=caller.getStringExtra("UserID");
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(mEntityName);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+      /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.single_entity_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        SEMapTab mSEMapTab;
+        SEChatsTab mSEChatsTab;
+        SEMembersTab mSEMembersTab;
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+
+            switch(position)
+            {
+                case 0:
+                    //     if(mSEMapTab==null)
+                    mSEMapTab= new SEMapTab();
+                    mSEMapTab.passUserDetails(mUserID,mUserName,mEntityName,mEntityID,mType);
+                    return mSEMapTab;
+                case 1:
+                    //     if(mSEChatsTab==null)
+                    mSEChatsTab= new SEChatsTab();
+                    return mSEChatsTab;
+                case 2:
+                    //    if(mSEMembersTab==null)
+
+                    /*
+                    This is where testing code starts
+
+                    Intent testActivity= new Intent();
+                    testActivity.setClass(getApplicationContext(),TestActivity.class);
+                    startActivity(testActivity);
+                    /*
+                    This is where testing code ends
+                     */
+
+                    mSEMembersTab= new SEMembersTab();
+                    mSEMembersTab.passUserDetails(mUserID,mUserName,mEntityName,mEntityID,mType);
+                    return mSEMembersTab;
+                default:
+                    return null;
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "MAP";
+                case 1:
+                    return "CHAT";
+                case 2:
+                    return "MEMBERS";
+            }
+            return null;
+        }
+    }
+}
