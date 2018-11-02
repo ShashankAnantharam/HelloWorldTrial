5a7838c6025809a2cec1a941d7e53ec93288cbfd
Shashank Anantharam <shashank95.a@gmail.com>
2018-11-03 00:05:01 +0530

    Helpers being written

---
+++ app/src/main/java/com/example/shashank_pc/trial/Helper/BasicHelper.java
@@ -0,0 +1,52 @@
+package com.example.shashank_pc.trial.Helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.shashank_pc.trial.classes.Alert;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BasicHelper {
+
+    public static List<Alert> populateAlerts(Map<String,Alert> alertMap)
+    {
+        List<Alert> alerts = new ArrayList<>();
+        for (Map.Entry<String, Alert> entry : alertMap.entrySet()) {
+            alerts.add(entry.getValue());
+        }
+        return alerts;
+    }
+
+    private static boolean getDatabaseConnectionStatus(Context context){
+        SharedPreferences preferences = context.getSharedPreferences("IS_FIREBASE_ONLINE", Context.MODE_PRIVATE);
+        return preferences.getBoolean("IS_FIREBASE_ONLINE", true);
+    }
+
+    public static void turnOnFirebaseDatabases(Context context){
+        if(!getDatabaseConnectionStatus(context)){
+            FirebaseDatabase.getInstance().goOnline();
+            FirebaseFirestore.getInstance().enableNetwork();
+            setDatabaseConnectionStatus(context,true);
+        }
+    }
+
+    public static void turnOffFirebaseDatabases(Context context, boolean isAppInForeground){
+        if(getDatabaseConnectionStatus(context) && !isAppInForeground){
+            FirebaseDatabase.getInstance().goOffline();
+            FirebaseFirestore.getInstance().disableNetwork();
+            setDatabaseConnectionStatus(context,false);
+        }
+    }
+
+    public static void setDatabaseConnectionStatus(Context context, boolean flag){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("IS_FIREBASE_ONLINE", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        edit.putBoolean("IS_FIREBASE_ONLINE",flag);
+        edit.commit();
+    }
+}
