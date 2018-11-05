c67fe154b16dfe6e1b41b8a09e3e4bbba0e950b4
Shashank Anantharam <shashank95.a@gmail.com>
2018-11-05 21:52:56 +0530

    partially done

--- app/src/main/java/com/example/shashank_pc/trial/Helper/AlertHelper.java
+++ app/src/main/java/com/example/shashank_pc/trial/Helper/AlertHelper.java
@@ -4,19 +4,32 @@ import com.example.shashank_pc.trial.classes.Alert;
 import com.example.shashank_pc.trial.classes.Lookout;
 import com.example.shashank_pc.trial.classes.Task;
 
+import java.util.Map;
+
 public class AlertHelper {
 
-    public static boolean shouldCheckAlert(Alert alert)
+    public static boolean shouldCheckAlert(Alert alert, Map<String,String> contactMap, String userID)
     {
         //TODO Fill this
         if(alert instanceof Lookout)
         {
-            if(!((Lookout) alert).isEnabled())
+            if((!((Lookout) alert).isEnabled())         //Lookout is not enabled
+                    ||
+                    ((!((Lookout) alert).getCreatedBy().equals(userID)) //Lookout is not created by user himself
+                            &&
+                            (!contactMap.containsKey(((Lookout) alert).getCreatedBy()) ||
+                                            !contactMap.get(((Lookout) alert).getCreatedBy()).equals("Y"))
+                            /*
+                            Lookout is either not created by a contact OR contact is not being broadcasted to
+                             */
+                    )
+                    )
             {
                 return false;
             }
 
-            //checkIfLookoutShouldTrigger
+            
+
 
         }
         else if(alert instanceof Task)
